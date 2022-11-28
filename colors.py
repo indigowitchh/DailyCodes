758bc64669e8005dc35c32412fe1edc4c34f3d58
do i give a darn <114239691+indigowitchh@users.noreply.github.com>
2022-11-28 08:25:25 -0700

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,7 @@
+import os
+os.system('color 7')
+end = input("Press any key to change colors")
+os.system('color f9')
+end = input("Press any key to change colors")
+os.system('color 2c')
+end = input("Press any key to quit")
